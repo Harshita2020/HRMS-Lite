d48b5cd3c217d2889eb2f824239e6ecf2cf2e6c8
Harshita2020 <harshitaautocad@gmail.com>
2026-02-25 23:53:28 +0530

    feat: implement full CRUD with delete integration in UI

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,26 +1,29 @@
-const Employee = ({ id, name, age, dept, salary }) => {
+const Employee = ({ id, name, age, dept, salary, onDelete }) => {
   return (
-    <div className="bg-white p-5 rounded-lg shadow-sm border border-gray-200">
-
+    <div className="bg-white p-5 rounded-lg shadow-sm border border-gray-200 flex flex-col">
       {/* Top Row */}
-      <div className="flex justify-between items-center mb-2">
-        <h2 className="text-lg font-semibold text-gray-800">
-          {name}
-        </h2>
-        <span className="text-green-600 font-bold">
-          ₹{salary}
-        </span>
+      <div className="flex justify-between items-center mb-3">
+        <h2 className="text-lg font-semibold text-gray-800">{name}</h2>
+        <span className="text-green-600 font-semibold">₹{salary}</span>
       </div>
 
       {/* Divider */}
-      <div className="border-t border-gray-200 my-2"></div>
+      <div className="border-t border-gray-200 my-3"></div>
 
       {/* Bottom Row */}
-      <div className="flex justify-between text-gray-600 text-sm">
+      <div className="flex justify-between text-sm text-gray-500">
         <span>Dept: {dept}</span>
         <span>Age: {age}</span>
       </div>
-
+      <div className="flex justify-center mt-5">
+        <button
+          className="bg-red-500 hover:bg-red-600 text-white text-sm font-medium px-8 py-2.5 rounded-lg transition w-fit"
+          onClick={onDelete}
+        >
+          {" "}
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/EmployeesList.jsx
+++ src/components/EmployeesList.jsx
@@ -11,10 +11,18 @@ const EmployeesList = () => {
       .catch((err) => console.log("ERROR!!!", err));
   }, []);
 
+  const handleDelete = (id) => {
+    fetch(`http://localhost:3000/employee/${id}`, {
+      method: "DELETE",
+    });
+    setData((prev) => prev.filter((emp) => emp.id !== id));
+  };
+
+  // const filteredData = data.filter()
+
   return (
-    <div className="min-h-screen bg-gray-100 py-10">
+    <div className="min-h-screen bg-gray-100 py-12">
       <div className="max-w-3xl mx-auto px-4">
-        
         {/* Header */}
         <h1 className="text-3xl font-bold text-blue-700 text-center mb-6">
           HRMS Dashboard
@@ -22,18 +30,25 @@ const EmployeesList = () => {
 
         {/* Button */}
         <div className="mb-6">
-          <button className="bg-green-500 hover:bg-green-600 text-white font-semibold px-5 py-2 rounded-lg shadow-sm transition-all duration-200">
+          <button className="bg-green-500 hover:bg-green-600 text-white text-center font-semibold px-5 py-2 rounded-lg shadow-sm transition-all duration-200 cursor-pointer">
             Add Employee
           </button>
         </div>
 
         {/* Employee Cards */}
-        <div className="space-y-4">
-          {data.map((d) => (
-            <Employee key={d.id} {...d} />
-          ))}
-        </div>
-
+        {data.length > 0 ? (
+          <div className="space-y-4">
+            {data.map((d) => (
+              <Employee key={d.id} {...d} onDelete={() => handleDelete(d.id)} />
+            ))}
+          </div>
+        ) : (
+          <div className="min-h-80 flex items-center justify-center">
+            <p className="text-gray-400 text-center">
+              Add your first employee to get started!
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
